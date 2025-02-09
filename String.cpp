8445820c915d28ec8d73ecdf5274047289920e04
remodik <slenderzet@gmail.com>
2025-02-09 18:14:25 +0300

    Initial commit

---
+++ String.cpp
@@ -0,0 +1,99 @@
+#include "String.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+String::String() : data(nullptr), size(0), capacity(0) {}
+
+String::String(const char* str) {
+    size = strlen(str);
+    capacity = size + 1;
+    data = new char[capacity];
+    strcpy(data, str);
+}
+
+String::String(const String& self) {
+    size = self.size;
+    capacity = self.capacity;
+    data = new char[capacity];
+    strcpy(data, self.data);
+}
+
+String::~String() { delete[] data; data = nullptr;}
+
+size_t String::length() const { return size; }
+
+bool String::empty() const { return size == 0; }
+
+void String::insert(const size_t pos, const String& subStr) {
+    if (pos > size) return;
+
+    const size_t _size = size + subStr.size;
+    resize(_size);
+
+    for (size_t i = size; i > pos; --i) {
+        data[i + subStr.size - 1] = data[i - 1];
+    }
+
+    for (size_t i = 0; i < subStr.size; ++i) {
+        data[pos + i] = subStr.data[i];
+    }
+    size = _size;
+}
+
+void String::erase(const size_t pos, const size_t count) {
+    if (pos + count > size) return;
+    for (size_t i = pos; i < size - count; ++i) {
+        data[i] = data[i + count];
+    }
+    size -= count;
+}
+
+void String::replace(const size_t pos, const size_t count, const String& subStr) {
+    erase(pos, count);
+    insert(pos, subStr);
+}
+
+void String::clear() {
+    delete[] data;
+    data = nullptr;
+    size = 0;
+    capacity = 0;
+}
+
+void String::concatenate(const String& self) {
+    const size_t _size = size + self.size;
+    resize(_size);
+
+    strcpy(data + size, self.data);
+    size = _size;
+}
+
+String String::substr(const size_t start, const size_t len) const {
+    if (start >= size) return {""};
+
+    const size_t alen = (start + len > size) ? size - start : len;
+    String subStr;
+    subStr.resize(alen);
+
+    for (size_t i = 0; i < alen; ++i) {
+        subStr.data[i] = data[start + i];
+    }
+
+    return subStr;
+}
+
+void String::resize(const size_t _size) {
+    if (_size > capacity) {
+        capacity = _size + 1;
+        auto _data = new char[capacity];
+        if (data != nullptr) {
+            strcpy(_data, data);
+            delete[] data;
+        }
+        data = _data;
+    }
+}
+
+void String::print() const {
+    cout << data << endl;
+}
---
+++ String.h
@@ -0,0 +1,29 @@
+#ifndef STRING_H
+#define STRING_H
+#include <iostream>
+
+class String {
+    char* data;
+    size_t size;
+    size_t capacity;
+
+    void resize(size_t _size);
+
+public:
+    String();
+    String(const char* str);
+    String(const String& self);
+    ~String();
+
+    size_t length() const;
+    bool empty() const;
+    void insert(size_t pos, const String& subStr);
+    void erase(size_t pos, size_t count);
+    void replace(size_t pos, size_t count, const String& subStr);
+    void clear();
+    void concatenate(const String& self);
+    String substr(size_t start, size_t len) const;
+    void print() const;
+};
+
+#endif
